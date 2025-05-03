226c1f37f956ceebd21c8d2166594fc5c9706aa3
Lyla Ibrahim <lylaibrahim@Lylas-MacBook-Pro-2.local>
2025-05-03 10:06:03 -0700

    added in profile photo and one profile aspect

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,21 @@
-function Profile() {
-    return (
+function ProfileHeader() {
+  return (
+    <div>
       <div>
         <h2>My Profile</h2>
-        <p>Dietary restrictions, class year, etc.</p>
+        <h3>Welcome Lyla Ibrahim!</h3>
+        <img 
+          src="/profilePhoto.jpeg" 
+          alt="Profile" 
+          className="profile-photo"
+        />
       </div>
-    );
-  }
-  export default Profile;
+
+      <div>
+        <h2>Food preferences</h2>
+      </div>
+    </div>
+  );
+}
+export default ProfileHeader;
+
