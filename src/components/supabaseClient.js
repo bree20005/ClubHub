feb02586c919bc7e521f5e270f6bd1f67e1d001e
bree20005 <96209035+bree20005@users.noreply.github.com>
2025-05-26 08:53:41 -0700

    Delete src/components/supabaseClient.js

--- src/components/supabaseClient.js
+++
@@ -1,7 +0,0 @@
-// src/supabase.js
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://your-project-id.supabase.co';
-const supabaseKey = 'your-public-anon-key';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
