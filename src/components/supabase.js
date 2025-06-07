f1c4fb79f4d8774cead0612704da492b4d7cbf38
Komal Vij <komalvij@stanford.edu>
2025-06-06 21:31:57 -0700

    api key hide

--- src/components/supabase.js
+++ src/components/supabase.js
@@ -2,6 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://ssjzaknoqwpegttnahqq.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNzanpha25vcXdwZWd0dG5haHFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcwMDc1MzAsImV4cCI6MjA2MjU4MzUzMH0.4KucsNCcqNZtCC-kzeBreWXzv0U3QbzbtOUm2PALpbY'; // from Project Settings > API
+// const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNzanpha25vcXdwZWd0dG5haHFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcwMDc1MzAsImV4cCI6MjA2MjU4MzUzMH0.4KucsNCcqNZtCC-kzeBreWXzv0U3QbzbtOUm2PALpbY'; // from Project Settings > API
+const supabaseKey = process.env.REACT_APP_MY_API_KEY;
+
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
--- src/helper/supabaseclient.js
+++ src/helper/supabaseclient.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://ssjzaknoqwpegttnahqq.supabase.co"
-const supabaseAnonnKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNzanpha25vcXdwZWd0dG5haHFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcwMDc1MzAsImV4cCI6MjA2MjU4MzUzMH0.4KucsNCcqNZtCC-kzeBreWXzv0U3QbzbtOUm2PALpbY"
-
+// const supabaseAnonnKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNzanpha25vcXdwZWd0dG5haHFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcwMDc1MzAsImV4cCI6MjA2MjU4MzUzMH0.4KucsNCcqNZtCC-kzeBreWXzv0U3QbzbtOUm2PALpbY"
+const supabaseAnonnKey = process.env.REACT_APP_MY_API_KEY;
 const supabase = createClient(supabaseUrl, supabaseAnonnKey);
 export default supabase;
