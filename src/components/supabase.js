a36ac71c691db0414decf159a537f4b4331812f3
ameshabanjara <abanjara@stanford.edu>
2025-06-06 22:31:22 -0700

    fixing

--- src/components/supabase.js
+++ src/components/supabase.js
@@ -2,6 +2,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://ssjzaknoqwpegttnahqq.supabase.co';
-const supabaseKey = process.env.SUPABASE_KEY
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNzanpha25vcXdwZWd0dG5haHFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcwMDc1MzAsImV4cCI6MjA2MjU4MzUzMH0.4KucsNCcqNZtCC-kzeBreWXzv0U3QbzbtOUm2PALpbY';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
